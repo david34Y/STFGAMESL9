74aa293b095796425970d730aa2535d18f2e2549
david34Y <dgonzalesn@pucp.edu.pe>
2022-11-15 20:43:39 -0500

    init

---
+++ src/main/java/com/example/lab91/Lab91Application.java
@@ -0,0 +1,13 @@
+package com.example.lab91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab91Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab91Application.class, args);
+    }
+
+}
