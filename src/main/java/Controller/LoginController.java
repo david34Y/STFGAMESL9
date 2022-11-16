0e0bca1133d1699f12f6126f4d3a27f9776b9759
david34Y <dgonzalesn@pucp.edu.pe>
2022-11-15 21:51:18 -0500

    Login Modified

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -9,9 +14,21 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
 
 
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "inicio";
+    }
 
     @GetMapping(value = "/login")
     public String login(HttpSession session){
         return "login";
     }
+
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+
 }
